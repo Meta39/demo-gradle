88a914072e44d0b226dcfba760f59a6762cdc682
Meta39 <53995537@qq.com>
2023-12-06 18:05:55 +0800

    模拟RestTemplate远程调用和异常处理

--- spring-boot-demo/src/test/java/com/fu/springboot/SpringbootDemoApplicationTests.java
+++ spring-boot-demo/src/test/java/com/fu/springboot/SpringbootDemoApplicationTests.java
@@ -1,14 +1,27 @@
 package com.fu.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @SpringBootTest
 public class SpringbootDemoApplicationTests {
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Test
     void test(){
-
+        try{
+            ResponseEntity<String> response = restTemplate.getForEntity("http://127.0.0.1:82/test", String.class);
+            log.info("body:{}", response.getBody());
+        } catch (HttpStatusCodeException e) {
+            log.error("HTTP Code:{}, Error Message: {}", e.getStatusCode().value(), e.getMessage());
+        }
     }
 
 }
