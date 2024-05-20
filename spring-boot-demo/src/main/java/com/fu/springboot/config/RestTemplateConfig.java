6fd4b31865da725714519ec79440db848e6e6460
Meta39 <53995537@qq.com>
2024-05-20 17:30:32 +0800

    RestTemplate使用Apache httpclient5连接池

--- spring-boot-demo/src/main/java/com/fu/springboot/config/RestTemplateConfig.java
+++ spring-boot-demo/src/main/java/com/fu/springboot/config/RestTemplateConfig.java
@@ -1,15 +1,46 @@
 package com.fu.springboot.config;
 
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate() {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        // 创建连接池管理器
+        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setMaxConnTotal(200) // 最大连接数
+                .setMaxConnPerRoute(20)// 每个路由的最大连接数
+                .build();
+
+        // 请求配置
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofMinutes(1))// 连接超时时间
+                .setResponseTimeout(Timeout.ofMinutes(1))// 设置响应超时时间
+                .build();
+
+        // 创建HttpClient对象
+        CloseableHttpClient client = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(client);
     }
 
 }
