a3c202722d1d023eff3a96605131918598dbf41b
Meta39 <53995537@qq.com>
2024-06-04 23:59:45 +0800

    1、增加编译参数 -parameters 防止使用@Cacheable、@CachePut、@CacheEvict缓存 null 值抛异常。【当然也可以使用unless手动判空，不缓存null值，一般建议null值也缓存】 2、使用 spring-boot-starter-cache + Caffeine 本地缓存

--- spring-boot-demo/src/main/java/com/fu/springboot/SpringbootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/fu/springboot/SpringbootDemoApplication.java
@@ -3,7 +3,9 @@ package com.fu.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching //启用缓存并使用 caffeine
 @MapperScan("com.fu.springboot.mapper")
 @SpringBootApplication
 public class SpringbootDemoApplication {
