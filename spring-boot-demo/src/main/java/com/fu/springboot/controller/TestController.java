2329e96328650596e4a3643cb673fed5c3159c95
Meta39 <53995537@qq.com>
2024-06-04 13:04:14 +0800

    SpringBoot启用虚拟线程提高吞吐量 在高吞吐量并发应用程序中使用虚拟线程，尤其是那些包含大量并发任务且大部分时间都在等待的应用程序。服务器应用程序是高吞吐量应用程序的示例，因为它们通常处理许多执行阻塞 I/O 操作（例如获取资源）的客户端请求。
    虚拟线程并不是更快的线程；它们运行代码的速度并不比平台线程快。它们存在的目的是为了提供规模（更高的吞吐量），而不是速度（更低的延迟）。

--- spring-boot-demo/src/main/java/com/fu/springboot/controller/TestController.java
+++ spring-boot-demo/src/main/java/com/fu/springboot/controller/TestController.java
@@ -1,10 +1,11 @@
 package com.fu.springboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 @RequestMapping("test")
 public class TestController {
@@ -16,6 +17,8 @@ public class TestController {
 //        throw new UnauthorizedException();//特殊异常，未认证异常，需要其它异常状态码区分。
 //        throw new ForbiddenException();//特殊异常，未鉴权异常，需要其它异常状态码区分。
 //        throw new MiniProgramException("小程序异常");//小程序异常，不需要状态码区分，但是需要区分异常类
+        String name = Thread.currentThread().getName();
+        log.info("name:{}", name);
         return "Hello world.";
     }
 
